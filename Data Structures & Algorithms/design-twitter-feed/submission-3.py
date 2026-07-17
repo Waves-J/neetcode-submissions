a376ea039da600b00c40ae8c04fe0a8c7a4c2ec3
Waves-J <wavesj04@gmail.com>
2026-07-17 15:22:59 -0700

    Add: design-twitter-feed - submission-3

---
+++ Data Structures & Algorithms/design-twitter-feed/submission-3.py
@@ -0,0 +1,49 @@
+import heapq
+
+class Twitter:
+
+    def __init__(self):
+        self.following = {}
+        self.posts = {}
+        self.time = 0
+    
+    def postTweet(self, userId: int, tweetId: int) -> None:
+        self.addUserToSystem(userId)
+        self.posts[userId][tweetId] = self.time
+        self.time += 1
+
+    def getNewsFeed(self, userId: int) -> List[int]:
+        maxheap = []
+        news_feed = []
+
+        for user in self.following[userId]:
+            for key, value in self.posts[user].items():
+                heapq.heappush_max(maxheap, (value, key))
+        
+        if userId not in self.following[userId]:
+            for key, value in self.posts[userId].items():
+                heapq.heappush_max(maxheap, (value, key))
+        
+        for _ in range(10):
+            if maxheap:
+                news_feed.append(heapq.heappop_max(maxheap)[1])
+            else:
+                break
+        
+        return news_feed
+
+    def follow(self, followerId: int, followeeId: int) -> None:
+        self.addUserToSystem(followerId)
+        self.addUserToSystem(followeeId)
+        self.following[followerId].add(followeeId)
+
+    def unfollow(self, followerId: int, followeeId: int) -> None:
+        self.addUserToSystem(followerId)
+        self.addUserToSystem(followeeId)
+        self.following[followerId].discard(followeeId)
+        
+    def addUserToSystem(self, userId: int) -> None:
+        if userId not in self.following:
+            self.following[userId] = set()
+        if userId not in self.posts:
+            self.posts[userId] = {}
