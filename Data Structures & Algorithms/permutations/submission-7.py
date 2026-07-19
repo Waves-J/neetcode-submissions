485c6705f2b672bf816bf827b3103c217a0206e8
Waves-J <wavesj04@gmail.com>
2026-07-19 10:44:54 -0700

    Add: permutations - submission-7

---
+++ Data Structures & Algorithms/permutations/submission-7.py
@@ -0,0 +1,15 @@
+class Solution:
+    def permute(self, nums: List[int]) -> List[List[int]]:
+
+        def backtrack(nums, idx):
+            if idx == len(nums):
+                self.res.append(nums[:])
+                return
+            for i in range(idx, len(nums)):
+                nums[idx], nums[i] = nums[i], nums[idx]
+                backtrack(nums, idx + 1)
+                nums[idx], nums[i] = nums[i], nums[idx]
+
+        self.res = []
+        backtrack(nums, 0)
+        return self.res
