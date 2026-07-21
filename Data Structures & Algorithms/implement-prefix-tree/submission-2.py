aef7c6a3ce9b41048e857ea40e087d7f6bf6e49c
Waves-J <wavesj04@gmail.com>
2026-07-21 11:20:32 -0700

    Add: implement-prefix-tree - submission-2

---
+++ Data Structures & Algorithms/implement-prefix-tree/submission-2.py
@@ -0,0 +1,37 @@
+class PrefixTree:
+
+    def __init__(self):
+        self.hashmap = {}
+
+    def insert(self, word: str) -> None:
+        curr = self.hashmap
+
+        for c in word:
+            if c not in curr:
+                curr[c] = {}
+            curr = curr[c]
+        
+        curr["exist"] = True
+
+        return
+            
+    def search(self, word: str) -> bool:
+        curr = self.hashmap
+
+        for c in word:
+            if c not in curr:
+                return False
+            curr = curr[c]
+        
+        return "exist" in curr
+
+    def startsWith(self, prefix: str) -> bool:
+        curr = self.hashmap
+
+        for c in prefix:
+            if c not in curr:
+                return False
+            curr = curr[c]
+        
+        return True
+        
