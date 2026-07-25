3f1f1c8ad4e0879e3460628553612e0325212187
Waves-J <wavesj04@gmail.com>
2026-07-24 22:58:37 -0700

    Add: clone-graph - submission-3

---
+++ Data Structures & Algorithms/clone-graph/submission-3.py
@@ -0,0 +1,29 @@
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val = 0, neighbors = None):
+        self.val = val
+        self.neighbors = neighbors if neighbors is not None else []
+"""
+
+class Solution:
+    def cloneGraph(self, node: Optional['Node']) -> Optional['Node']:
+        if not node:
+            return None
+
+        old_to_new = {}
+        def dfs(v):
+            if v in old_to_new:
+                return old_to_new[v]
+
+            new = Node(v.val)
+            old_to_new[v] = new
+
+            for u in v.neighbors:
+                new.neighbors.append(dfs(u))
+                
+            return new
+
+        return dfs(node)
+
+
