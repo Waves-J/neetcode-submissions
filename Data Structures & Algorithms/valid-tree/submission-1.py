75ff4c9d7da2d27eeba7a635a59a179c8afa3c50
Waves-J <wavesj04@gmail.com>
2026-07-27 19:44:12 -0700

    Add: valid-tree - submission-1

---
+++ Data Structures & Algorithms/valid-tree/submission-1.py
@@ -0,0 +1,32 @@
+class Solution:
+    def validTree(self, n: int, edges: List[List[int]]) -> bool:
+        linked = {}
+        self.isCycle = False
+        gray = set()
+        black = set()
+        
+        for i in range(n):
+            linked[i] = []
+        for edge in edges:
+            linked[edge[1]].append(edge[0])
+            linked[edge[0]].append(edge[1])
+
+        def dfs(v, p):
+            if v in black:
+                return
+
+            gray.add(v)
+            for edge in linked[v]:
+                if edge in gray and edge != p:
+                    self.isCycle = True
+                    return False
+                elif edge not in black and edge != p:
+                    if not dfs(edge, v):
+                        return False
+            
+            gray.remove(v)
+            black.add(v)
+            return True
+
+
+        return dfs(0, None) and (len(black) == n)
