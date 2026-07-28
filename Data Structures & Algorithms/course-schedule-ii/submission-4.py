bd6e79b501ab60406704d41896a66bcb518ca7d9
Waves-J <wavesj04@gmail.com>
2026-07-27 18:56:33 -0700

    Add: course-schedule-ii - submission-4

---
+++ Data Structures & Algorithms/course-schedule-ii/submission-4.py
@@ -0,0 +1,33 @@
+class Solution:
+    def findOrder(self, numCourses: int, prerequisites: List[List[int]]) -> List[int]:
+        linked = {}
+        self.isCycle = False
+        order = []
+        gray = set()
+        black = set()
+        
+        for i in range(numCourses):
+            linked[i] = set()
+        for prereq in prerequisites:
+            linked[prereq[1]].add(prereq[0])
+
+        def dfs(v):
+            if v in black:
+                return
+
+            gray.add(v)
+            for prereq in linked[v]:
+                if prereq in gray:
+                    self.isCycle = True
+                elif prereq not in black:
+                    dfs(prereq)
+            
+            gray.remove(v)
+            order.append(v)
+            black.add(v)
+        
+        for i in range(numCourses):
+            dfs(i)
+        
+        order.reverse()
+        return order if not self.isCycle else []
