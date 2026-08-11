6c8c7678f143f6f3f9de240aed02004d4d46f706
Waves-J <wavesj04@gmail.com>
2026-08-11 18:04:23 +0800

    Add: house-robber - submission-2

---
+++ Data Structures & Algorithms/house-robber/submission-2.py
@@ -0,0 +1,23 @@
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        rob1, rob2 = 0, 0
+
+        for num in nums:
+            temp = max(num + rob1, rob2)
+            rob1 = rob2
+            rob2 = temp
+
+        return rob2
+
+        # n = len(nums)
+        # dp = [-1] * len(nums)
+
+        # def dfs(i):
+        #     if i >= n:
+        #         return 0
+        #     if dp[i] != -1:
+        #         return dp[i]
+        #     dp[i] = nums[i] + max(dfs(i + 2), dfs(i + 3))
+        #     return dp[i]
+        
+        # return max(dfs(0), dfs(1))  
